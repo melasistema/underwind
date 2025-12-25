99cd077106601e6d264b5cdfd191ca4a732b6f18
Luca Visciola <info@melasistema.com>
2025-12-25 18:00:42 +0100

    fix(cors): Fix mixed content

--- functions.php
+++ functions.php
@@ -69,6 +69,34 @@ function underwind_setup() {
 }
 add_action( 'after_setup_theme', 'underwind_setup' );
 
+
+/**
+ * Fix mixed content issues on SSL.
+ */
+function underwind_ssl_output_buffer_start() {
+	if ( is_ssl() ) {
+		ob_start( 'underwind_ssl_output_buffer_callback' );
+	}
+}
+
+function underwind_ssl_output_buffer_callback( $buffer ) {
+	$site_url        = get_option( 'siteurl' );
+	$site_url_parsed = wp_parse_url( $site_url );
+	$site_domain     = $site_url_parsed['host'];
+
+	return str_replace( 'http://' . $site_domain, 'https://' . $site_domain, $buffer );
+}
+
+function underwind_ssl_output_buffer_end() {
+	if ( is_ssl() && ob_get_length() > 0 ) {
+		ob_end_flush();
+	}
+}
+
+add_action( 'init', 'underwind_ssl_output_buffer_start' );
+add_action( 'shutdown', 'underwind_ssl_output_buffer_end' );
+
+
 // --- Vite & Tailwind Enqueue ---
 define( 'UNDERWIND_VITE_DEV_SERVER_URL', 'http://localhost:5173' );
 define( 'UNDERWIND_VITE_ENTRY_POINT', 'src/js/app.js' );
